21885a3693c802df26f5621381a6cf9f91e4716b
Hassna-bhd <Has1020sna@gmail.com>
2023-03-27 08:15:13 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /** 
  *_strlen - returns the length of a string
  *@s: string
@@ -8,7 +9,7 @@ int _strlen(char *s)
 {
 	int longi = 0;
 
-	while(*s != '\0')
+	while (*s != '\0')
 	{
 		longi++;
 		s++;
